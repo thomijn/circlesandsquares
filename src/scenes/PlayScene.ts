a285d758ac8371cbf90e77a8e5d90238a95d4589
Thomas Gertenbach <Thomas.gertenbach99@gmail.com>
2019-05-20 17:02:41 +0200

    testmap and charachter
    added a testmap with basic character movement

--- src/scenes/PlayScene.ts
+++ src/scenes/PlayScene.ts
@@ -2,31 +2,90 @@ import { CST } from "../CST";
 
 
 export class PlayScene extends Phaser.Scene {
-    
+    Keyboard: any;
+    player!: Phaser.Physics.Arcade.Sprite;
     constructor() {
         super({
             key: CST.SCENES.PLAY,
         });
     }
     preload() {
-        
-        this.load.image("Dungeon", "./assets/image/Dungeon.png");
-        this.load.tilemapTiledJSON("mappy", "./assets/maps/mappy.json");
+
+        this.load.image("Dungeon", "./assets/image/tileset_dungeon.png");
+        this.load.tilemapTiledJSON("mappy", "./assets/maps/level_01..json");
+
+        this.load.spritesheet('dude',
+            './assets/image/character.png',
+            { frameWidth: 32, frameHeight: 32 }
+        );
     }
 
     create() {
-        
+
         let mappy = this.add.tilemap("mappy");
 
-        let terrain = mappy.addTilesetImage("Dungeon", "Dungeon");
+        let terrain = mappy.addTilesetImage("tile_set", "Dungeon");
         // let itemset = mappy.addTilesetImage("items");
 
         //layers
-        let botLayer = mappy.createStaticLayer("bottom", [terrain], 0, 0).setDepth(-2);
-        let middleLayer = mappy.createStaticLayer("floor", [terrain], 0, 0).setDepth(-1);
-        let topLayer = mappy.createStaticLayer("wall", [terrain], 0, 0).setDepth(0);
-        let objectsLayer = mappy.createStaticLayer("objects", [terrain], 0, 0).setDepth(1);
 
+        let ground = mappy.createStaticLayer("Ground", [terrain], 0, 0).setDepth(0);
+        let holes = mappy.createStaticLayer("Holes", [terrain], 0, 0).setDepth(1);
+
+        let blocks = mappy.createStaticLayer("Blocks", [terrain], 0, 0).setDepth(2);
+        let walls = mappy.createStaticLayer("Walls", [terrain], 0, 0).setDepth(3);
+        let entrance = mappy.createStaticLayer("Entrance", [terrain], 0, 0).setDepth(4);
+
+
+
+
+        this.player = this.physics.add.sprite(100, 450, 'dude').setDepth(5);
+
+        this.anims.create({
+            key: 'walk',
+            frames: this.anims.generateFrameNumbers('dude', { start: 3, end: 5 }),
+            frameRate: 10,
+
+        });
+
+        this.Keyboard = this.input.keyboard.addKeys("W, A, S, D")
+
+    }
+
+    update(time: number, delta: number) {
+
+        if (this.Keyboard.D.isDown === true) {
+            this.player.setVelocityX(64);
+            this.player.play("walk", true);
+            this.player.flipX = false;
+        } 
+        
+        if(this.Keyboard.A.isDown === true) {
+            this.player.setVelocityX(-64);
+            this.player.play("walk", true);
+            this.player.flipX = true;
+        }
+
+        if(this.Keyboard.W.isDown === true) {
+            this.player.setVelocityY(-64);
+            this.player.play("walk", true);
+        }
+
+        if(this.Keyboard.S.isDown === true) {
+            this.player.setVelocityY(64);
+            this.player.play("walk", true);
+        }
+
+        if(this.Keyboard.A.isUp && this.Keyboard.D.isUp){
+            this.player.setVelocityX(0);
+        }
+
+        
+        if(this.Keyboard.S.isUp && this.Keyboard.W.isUp){
+            this.player.setVelocityY(0);
+        }
+
+        
 
     }
 }
